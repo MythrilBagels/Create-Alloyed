8d0a941929f0a81f5df02ded54400ce7361acb03
FortressNebula <79470293+FortressNebula@users.noreply.github.com>
2021-12-04 11:14:51 +0000

    Added steel sheet

--- src/main/java/com/spottytheturtle/alloyed/items/ModItems.java
+++ src/main/java/com/spottytheturtle/alloyed/items/ModItems.java
@@ -4,6 +4,7 @@ import com.spottytheturtle.alloyed.Alloyed;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -12,10 +13,15 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Alloyed.MOD_ID);
 
 
+    // Bronze
     public static final RegistryObject<Item> BRONZE_INGOT = ITEMS.register("bronze_ingot",
             () -> new Item(new Item.Properties().tab(ModItemGroup.TEST_GROUP)));
 
+    // Steel
     public static final RegistryObject<Item> STEEL_INGOT = ITEMS.register("steel_ingot",
+            () -> new Item(new Item.Properties().tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_SHEET = ITEMS.register("steel_sheet",
             () -> new Item(new Item.Properties().tab(ModItemGroup.TEST_GROUP)));
 
 
