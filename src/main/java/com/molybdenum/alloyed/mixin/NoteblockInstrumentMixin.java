af9cae6462f00d5bab5c92668118995f8f9424a7
FortressNebula <79470293+FortressNebula@users.noreply.github.com>
2021-12-09 17:38:59 +0000

    more mixin attempts

--- src/main/java/com/molybdenum/alloyed/mixin/NoteblockInstrumentMixin.java
+++ src/main/java/com/molybdenum/alloyed/mixin/NoteblockInstrumentMixin.java
@@ -23,23 +23,23 @@ public abstract class NoteblockInstrumentMixin {
     @Mutable
     private static NoteBlockInstrument[] $VALUES;
 
-    private static final NoteBlockInstrument BRONZE_BELL = addVariant("BRONZE_BELL", "bronze_bell", ModSounds.BRONZE_BELL.get());
+    private static final NoteBlockInstrument BRONZE_BELL = alloyed$addVariant("BRONZE_BELL", "bronze_bell", ModSounds.BRONZE_BELL.get());
 
     @Invoker("<init>")
-    public static NoteBlockInstrument invokeInit(String internalName, int internalId, String name, SoundEvent sound) {
+    public static NoteBlockInstrument alloyed$invokeInit(String internalName, int internalId, String name, SoundEvent sound) {
         throw new AssertionError();
     }
 
-    private static NoteBlockInstrument addVariant(String internalName, String name, SoundEvent sound) {
+    private static NoteBlockInstrument alloyed$addVariant(String internalName, String name, SoundEvent sound) {
         ArrayList<NoteBlockInstrument> variants = new ArrayList<NoteBlockInstrument>(Arrays.asList(NoteblockInstrumentMixin.$VALUES));
-        NoteBlockInstrument instrument = invokeInit(internalName, variants.get(variants.size() - 1).ordinal() + 1, name, sound);
+        NoteBlockInstrument instrument = alloyed$invokeInit(internalName, variants.get(variants.size() - 1).ordinal() + 1, name, sound);
         variants.add(instrument);
         NoteblockInstrumentMixin.$VALUES = variants.toArray(new NoteBlockInstrument[0]);
         return instrument;
     }
 
-    @Inject(method = "byState", at = @At("HEAD"), cancellable = true)
-    private static void byState(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> ci) {
+    @Inject(method = "byState", at = @At("HEAD"), cancellable = true, remap = false)
+    private static void alloyed$byState(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> ci) {
         if (state.is(ModBlocks.BRONZE_BELL.get())) {
             ci.setReturnValue(BRONZE_BELL);
         }
