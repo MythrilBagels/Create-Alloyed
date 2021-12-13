d196ba1c7bb4609dda68d8496b8db73735644db5
FortressNebula <79470293+FortressNebula@users.noreply.github.com>
2021-12-13 21:34:35 +0000

    rebuilt and fixed some bugs

---
+++ build/tmp/expandedArchives/forge-1.16.5-36.2.8-inject_src.jar_b7cb56c54338f77ccec467a6639f2485/net/minecraft/util/math/vector/package-info.java
@@ -0,0 +1,7 @@
+// Auto generated package-info by MCP
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package net.minecraft.util.math.vector;
+
+import mcp.MethodsReturnNonnullByDefault;
+import javax.annotation.ParametersAreNonnullByDefault;
