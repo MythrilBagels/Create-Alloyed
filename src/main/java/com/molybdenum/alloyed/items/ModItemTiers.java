76730d35bd2cb7152deae124fdeddb3072a7f637
FortressNebula <79470293+FortressNebula@users.noreply.github.com>
2021-12-14 18:02:39 +0000

    Added steel toolset

--- src/main/java/com/molybdenum/alloyed/items/ModItemTiers.java
+++ src/main/java/com/molybdenum/alloyed/items/ModItemTiers.java
@@ -1,5 +1,61 @@
 package com.molybdenum.alloyed.items;
 
-public enum ModItemTiers {
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.util.LazyValue;
 
+import java.util.function.Supplier;
+
+public enum ModItemTiers implements IItemTier {
+    STEEL(3, 1000, 7.0F, 3.0F, 11, () -> {
+        return Ingredient.of(ModItems.STEEL_INGOT.get());
+    });
+
+    // Base code for item tier
+
+    private final int level;
+    private final int uses;
+    private final float speed;
+    private final float damage;
+    private final int enchantmentValue;
+    private final LazyValue<Ingredient> repairIngredient;
+
+    ModItemTiers(int level, int uses, float speed, float damage, int enchantmentValue, Supplier<Ingredient> repairIngredient) {
+        this.level = level;
+        this.uses = uses;
+        this.speed = speed;
+        this.damage = damage;
+        this.enchantmentValue = enchantmentValue;
+        this.repairIngredient = new LazyValue<>(repairIngredient);
+    }
+
+    @Override
+    public int getUses() {
+        return uses;
+    }
+
+    @Override
+    public float getSpeed() {
+        return speed;
+    }
+
+    @Override
+    public float getAttackDamageBonus() {
+        return damage;
+    }
+
+    @Override
+    public int getLevel() {
+        return level;
+    }
+
+    @Override
+    public int getEnchantmentValue() {
+        return enchantmentValue;
+    }
+
+    @Override
+    public Ingredient getRepairIngredient() {
+        return repairIngredient.get();
+    }
 }
--- src/main/java/com/molybdenum/alloyed/items/ModItems.java
+++ src/main/java/com/molybdenum/alloyed/items/ModItems.java
@@ -1,7 +1,7 @@
 package com.molybdenum.alloyed.items;
 
 import com.molybdenum.alloyed.Alloyed;
-import net.minecraft.item.Item;
+import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -26,13 +26,44 @@ public class ModItems {
     public static final RegistryObject<Item> STEEL_SHEET = ITEMS.register("steel_sheet",
             () -> new Item(new Item.Properties().tab(ModItemGroup.TEST_GROUP)));
 
+    // Steel tools
+    public static final RegistryObject<Item> STEEL_SWORD = ITEMS.register("steel_sword",
+            () -> new SwordItem(ModItemTiers.STEEL, 3, -2.4F, (new Item.Properties())
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_SHOVEL = ITEMS.register("steel_shovel",
+            () -> new ShovelItem(ModItemTiers.STEEL, 1.5F, -3.0F, (new Item.Properties())
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_PICKAXE = ITEMS.register("steel_pickaxe",
+            () -> new PickaxeItem(ModItemTiers.STEEL, 1, -2.8F, (new Item.Properties())
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_AXE = ITEMS.register("steel_axe",
+            () -> new AxeItem(ModItemTiers.STEEL, 5.0F, -3.0F, (new Item.Properties())
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_HOE = ITEMS.register("steel_hoe",
+            () -> new HoeItem(ModItemTiers.STEEL, -3, 0.0F, (new Item.Properties())
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_SHEARS = ITEMS.register("steel_shears",
+            () -> new ShearsItem(new Item.Properties()
+                    .durability(476)
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+    public static final RegistryObject<Item> STEEL_FISHING_ROD = ITEMS.register("steel_fishing_rod",
+            () -> new FishingRodItem(new Item.Properties()
+                    .durability(128)
+                    .tab(ModItemGroup.TEST_GROUP)));
+
+
+
     //public static final RegistryObject<Item> OIL_BUCKET = ITEMS.register("oil_bucket",
     //        () -> new BucketItem(() -> ModFluids.OIL_FLUID.get(), new Item.Properties().maxStackSize(1).group(ModItemGroup.TEST_GROUP)));
 
 
-    public static void register(IEventBus eventBus) {
-        ITEMS.register(eventBus);
-    }
+    public static void register(IEventBus eventBus) { ITEMS.register(eventBus); }
 
 
 }
