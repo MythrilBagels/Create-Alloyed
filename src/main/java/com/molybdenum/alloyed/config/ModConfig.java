a604684134d918db80a4e6a7ea5dc0b7523830d4
FortressNebula <79470293+FortressNebula@users.noreply.github.com>
2021-12-17 16:34:43 +0000

    Create ModConfig.java

---
+++ src/main/java/com/molybdenum/alloyed/config/ModConfig.java
@@ -0,0 +1,24 @@
+package com.molybdenum.alloyed.config;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public final class ModConfig {
+
+    public static final ForgeConfigSpec.Builder SERVER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SERVER_CONF;
+
+    // Recipe Settings
+    public static ForgeConfigSpec.ConfigValue<Boolean> OVERRIDE_STEAMPOWERED_RECIPES;
+
+    static {
+        SERVER.comment("Recipe Settings").push("recipe");
+        OVERRIDE_STEAMPOWERED_RECIPES = SERVER
+                .comment("Overrides Create: Steam Powered's bronze recipes to use bronze instead of brass. False by default.")
+                .define("overrideSteamPoweredRecipes", false);
+        SERVER.pop();
+
+        // Finished building config!
+        SERVER_CONF = SERVER.build();
+    }
+
+}
