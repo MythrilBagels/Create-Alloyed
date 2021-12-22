f42bfd69785710a0f4e7cd02035ee9eb14a327e8
FortressNebula <prasun.nandan0909@gmail.com>
2021-12-22 13:06:12 +0000

    Finished converting mod items to use registrates

--- src/main/java/com/molybdenum/alloyed/items/ModItems.java
+++ src/main/java/com/molybdenum/alloyed/items/ModItems.java
@@ -85,6 +85,14 @@ public class ModItems {
 
     public static final ItemEntry<Item> BRONZE_SHEET;
     public static final ItemEntry<Item> STEEL_SHEET;
+
+    public static final ItemEntry<SwordItem> STEEL_SWORD;
+    public static final ItemEntry<PickaxeItem> STEEL_PICKAXE;
+    public static final ItemEntry<AxeItem> STEEL_AXE;
+    public static final ItemEntry<ShovelItem> STEEL_SHOVEL;
+    public static final ItemEntry<HoeItem> STEEL_HOE;
+    public static final ItemEntry<ShearsItem> STEEL_SHEARS;
+    public static final ItemEntry<FishingRodItem> STEEL_FISHING_ROD;
     // End Item Entries
 
     public ModItems() {}
@@ -95,6 +103,14 @@ public class ModItems {
 
         Create.registrate().addToSection(BRONZE_SHEET, AllSections.MATERIALS);
         Create.registrate().addToSection(STEEL_SHEET, AllSections.MATERIALS);
+
+        Create.registrate().addToSection(STEEL_SWORD, AllSections.CURIOSITIES);
+        Create.registrate().addToSection(STEEL_PICKAXE, AllSections.CURIOSITIES);
+        Create.registrate().addToSection(STEEL_AXE, AllSections.CURIOSITIES);
+        Create.registrate().addToSection(STEEL_SHOVEL, AllSections.CURIOSITIES);
+        Create.registrate().addToSection(STEEL_HOE, AllSections.CURIOSITIES);
+        Create.registrate().addToSection(STEEL_SHEARS, AllSections.CURIOSITIES);
+        Create.registrate().addToSection(STEEL_FISHING_ROD, AllSections.CURIOSITIES);
     }
 
     // Functions from Create's item registrate. Found them useful, so copied them over.
@@ -113,5 +129,40 @@ public class ModItems {
 
         BRONZE_SHEET = taggedIngredient("bronze_sheet", ModTags.Items.BRONZE_SHEET);
         STEEL_SHEET = taggedIngredient("steel_sheet", ModTags.Items.STEEL_SHEET);
+
+        STEEL_SWORD = REGISTRATE
+                .object("steel_sword")
+                .item((p) -> new SwordItem(ModItemTiers.STEEL, 3, -2.4F, p))
+                .register();
+
+        STEEL_PICKAXE = REGISTRATE
+                .object("steel_pickaxe")
+                .item((p) -> new PickaxeItem(ModItemTiers.STEEL, 1, -2.8F,p))
+                .register();
+
+        STEEL_AXE = REGISTRATE
+                .object("steel_axe")
+                .item((p) -> new AxeItem(ModItemTiers.STEEL, 5.0F, -3.0F,p))
+                .register();
+
+        STEEL_SHOVEL = REGISTRATE
+                .object("steel_shovel")
+                .item((p) -> new ShovelItem(ModItemTiers.STEEL, 1.5F, -3.0F, p))
+                .register();
+
+        STEEL_HOE = REGISTRATE
+                .object("steel_hoe")
+                .item((p) -> new HoeItem(ModItemTiers.STEEL, -3, 0.0F, p))
+                .register();
+
+        STEEL_SHEARS = REGISTRATE
+                .object("steel_shears")
+                .item((p) -> new ShearsItem(p.durability(476)))
+                .register();
+
+        STEEL_FISHING_ROD = REGISTRATE
+                .object("steel_fishing_rod")
+                .item((p) -> new FishingRodItem(p.durability(128)))
+                .register();
     }
 }
