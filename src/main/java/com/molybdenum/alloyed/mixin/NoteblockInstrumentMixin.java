29a20b6efcc4bf5d575244e01de856ca64fbcc62
FortressNebula <prasun.nandan0909@gmail.com>
2021-12-23 14:26:11 +0000

    Revert "Moved Noteblock instrument definition to ModSounds"
    This reverts commit 436991a00d63136710a32a4f63e8519eb7b6b5c8.

--- src/main/java/com/molybdenum/alloyed/mixin/NoteblockInstrumentMixin.java
+++ src/main/java/com/molybdenum/alloyed/mixin/NoteblockInstrumentMixin.java
@@ -20,7 +20,13 @@ import java.lang.reflect.Modifier;
 @Mixin(NoteBlockInstrument.class)
 @Unique
 public abstract class NoteblockInstrumentMixin {
-
+    static {
+        try {
+            ModSounds.BRONZE_BELL_NOTEBLOCK = addToEnum(createNoteBlockInstrument("BRONZE_BELL", "bronze_bell"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Inject(method = "byState", at = @At("HEAD"), cancellable = true)
     private static void alloyed$byState(BlockState state, CallbackInfoReturnable<NoteBlockInstrument> ci) {
@@ -29,6 +35,34 @@ public abstract class NoteblockInstrumentMixin {
         }
     }
 
+    private static NoteBlockInstrument createNoteBlockInstrument(String internalName, String soundName) throws Exception {
+        Constructor<?> constructor = NoteBlockInstrument.class.getDeclaredConstructor(String.class, int.class, String.class, SoundEvent.class);
+        constructor.setAccessible(true);
+        Field constructorAccessorField = Constructor.class.getDeclaredField("constructorAccessor");
+        constructorAccessorField.setAccessible(true);
+        ConstructorAccessor ca = (ConstructorAccessor) constructorAccessorField.get(constructor);
+        if (ca == null) {
+            Method acquireConstructorAccessorMethod = Constructor.class.getDeclaredMethod("acquireConstructorAccessor");
+            acquireConstructorAccessorMethod.setAccessible(true);
+            ca = (ConstructorAccessor) acquireConstructorAccessorMethod.invoke(constructor);
+        }
+        // note that real constructor contains 2 additional parameters, name and ordinal
+        NoteBlockInstrument enumValue = (NoteBlockInstrument) ca.newInstance(new Object[]{internalName, NoteBlockInstrument.values().length + 1, soundName, null});
+        return enumValue;
+    }
 
+    private static NoteBlockInstrument addToEnum(NoteBlockInstrument instrument) throws Exception {
+        Field values = NoteBlockInstrument.class.getDeclaredField("$VALUES");
+        values.setAccessible(true);
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(values, values.getModifiers() & ~Modifier.FINAL);
+        NoteBlockInstrument[] oldValues = (NoteBlockInstrument[]) values.get(null);
+        NoteBlockInstrument[] newValues = new NoteBlockInstrument[oldValues.length + 1];
+        System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
+        newValues[oldValues.length] = instrument;
+        values.set(null, newValues);
+        return instrument;
+    }
 
 }
--- src/main/java/com/molybdenum/alloyed/sounds/ModSounds.java
+++ src/main/java/com/molybdenum/alloyed/sounds/ModSounds.java
@@ -8,13 +8,8 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import sun.reflect.ConstructorAccessor;
 
 import javax.annotation.Nullable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 import java.util.function.Consumer;
 
 public class ModSounds {
@@ -22,13 +17,6 @@ public class ModSounds {
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Alloyed.MOD_ID);
     @Nullable
     public static NoteBlockInstrument BRONZE_BELL_NOTEBLOCK;
-    static {
-        try {
-            BRONZE_BELL_NOTEBLOCK = addToEnum(createNoteBlockInstrument("BRONZE_BELL", "bronze_bell"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public static final RegistryObject<SoundEvent> BRONZE_BELL = registerSoundEvent("bronze_bell", soundEvent -> BRONZE_BELL_NOTEBLOCK.soundEvent = soundEvent);
 
@@ -43,35 +31,5 @@ public class ModSounds {
 
     public static void register(IEventBus eventBus) {
         SOUND_EVENTS.register(eventBus);
-    }
-
-    private static NoteBlockInstrument createNoteBlockInstrument(String internalName, String soundName) throws Exception {
-        Constructor<?> constructor = NoteBlockInstrument.class.getDeclaredConstructor(String.class, int.class, String.class, SoundEvent.class);
-        constructor.setAccessible(true);
-        Field constructorAccessorField = Constructor.class.getDeclaredField("constructorAccessor");
-        constructorAccessorField.setAccessible(true);
-        ConstructorAccessor ca = (ConstructorAccessor) constructorAccessorField.get(constructor);
-        if (ca == null) {
-            Method acquireConstructorAccessorMethod = Constructor.class.getDeclaredMethod("acquireConstructorAccessor");
-            acquireConstructorAccessorMethod.setAccessible(true);
-            ca = (ConstructorAccessor) acquireConstructorAccessorMethod.invoke(constructor);
-        }
-        // note that real constructor contains 2 additional parameters, name and ordinal
-        NoteBlockInstrument enumValue = (NoteBlockInstrument) ca.newInstance(new Object[]{internalName, NoteBlockInstrument.values().length + 1, soundName, null});
-        return enumValue;
-    }
-
-    private static NoteBlockInstrument addToEnum(NoteBlockInstrument instrument) throws Exception {
-        Field values = NoteBlockInstrument.class.getDeclaredField("$VALUES");
-        values.setAccessible(true);
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(values, values.getModifiers() & ~Modifier.FINAL);
-        NoteBlockInstrument[] oldValues = (NoteBlockInstrument[]) values.get(null);
-        NoteBlockInstrument[] newValues = new NoteBlockInstrument[oldValues.length + 1];
-        System.arraycopy(oldValues, 0, newValues, 0, oldValues.length);
-        newValues[oldValues.length] = instrument;
-        values.set(null, newValues);
-        return instrument;
     }
 }
