d21411732d8b075c739e42e21d2ae49f27d08874
FortressNebula <prasun.nandan0909@gmail.com>
2021-12-27 19:18:47 +0000

    Added tags for bronze and steel blocks
    Not sure how necessary lines 23-26 are in ModTags.java, but yeah

--- src/main/java/com/molybdenum/alloyed/util/ModTags.java
+++ src/main/java/com/molybdenum/alloyed/util/ModTags.java
@@ -1,7 +1,9 @@
 package com.molybdenum.alloyed.util;
 
 import com.molybdenum.alloyed.Alloyed;
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
@@ -18,12 +20,31 @@ public class ModTags {
         public static final Tags.IOptionalNamedTag<Item> BRONZE_SHEET = createForgeTag("plates/bronze");
         public static final Tags.IOptionalNamedTag<Item> STEEL_SHEET = createForgeTag("plates/steel");
 
+        // Compat for other mods' ingot blocks
+        public static final Tags.IOptionalNamedTag<Item> BRONZE_BLOCK = createForgeTag("storage_blocks/bronze");
+        public static final Tags.IOptionalNamedTag<Item> STEEL_BLOCK = createForgeTag("storage_blocks/steel");
+
         private static Tags.IOptionalNamedTag<Item> createTag(String name) {
             return ItemTags.createOptional(new ResourceLocation(Alloyed.MOD_ID, name));
         }
 
         private static Tags.IOptionalNamedTag<Item> createForgeTag(String name) {
             return ItemTags.createOptional(new ResourceLocation("forge", name));
+        }
+    }
+
+    public static class Blocks {
+
+        // Compat for other mods' ingot blocks
+        public static final Tags.IOptionalNamedTag<Block> BRONZE_BLOCK = createForgeTag("storage_blocks/bronze");
+        public static final Tags.IOptionalNamedTag<Block> STEEL_BLOCK = createForgeTag("storage_blocks/steel");
+
+        private static Tags.IOptionalNamedTag<Block> createTag(String name) {
+            return BlockTags.createOptional(new ResourceLocation(Alloyed.MOD_ID, name));
+        }
+
+        private static Tags.IOptionalNamedTag<Block> createForgeTag(String name) {
+            return BlockTags.createOptional(new ResourceLocation("forge", name));
         }
     }
 
