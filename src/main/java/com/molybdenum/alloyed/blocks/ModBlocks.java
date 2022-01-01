2520aab13361882d8203e867e9ef82bb9a6f6c7f
FortressNebula <prasun.nandan0909@gmail.com>
2022-01-01 19:57:40 +0000

    Removed the comment

--- src/main/java/com/molybdenum/alloyed/blocks/ModBlocks.java
+++ src/main/java/com/molybdenum/alloyed/blocks/ModBlocks.java
@@ -23,67 +23,6 @@ import javax.annotation.Nonnull;
 
 import static com.simibubi.create.AllTags.tagBlockAndItem;
 
-/* Leaving the main class in here for safe keeping
-public class ModBlocks {
-
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Alloyed.MOD_ID);
-
-
-    // Bronze
-    public static final RegistryObject<Block> BRONZE_BLOCK = registerBlock("bronze_block",
-            () -> new Block(AbstractBlock.Properties
-                    .of(Material.METAL)
-                    .harvestLevel(2)
-                    .harvestTool(ToolType.PICKAXE)
-                    .requiresCorrectToolForDrops()
-                    .strength(5f)));
-
-    public static final RegistryObject<Block> BRONZE_BELL = registerBlock("bronze_bell",
-            () -> new Block(AbstractBlock.Properties
-                    .of(Material.METAL)
-                    .harvestLevel(1)
-                    .harvestTool(ToolType.PICKAXE)
-                    .requiresCorrectToolForDrops()
-                    .strength(3f)
-                    .sound(SoundType.ANVIL)
-                    .noOcclusion()));
-
-    // Steel
-    public static final RegistryObject<Block> STEEL_BLOCK = registerBlock("steel_block",
-            () -> new Block(AbstractBlock.Properties
-                    .of(Material.METAL)
-                    .harvestLevel(2)
-                    .harvestTool(ToolType.PICKAXE)
-                    .requiresCorrectToolForDrops()
-                    .strength(6f)));
-
-
-
-    //public static final RegistryObject<Block> LIGHTNING_CHANNELER = registerBlock("lightning_channeler",
-    //        () -> new LightningChannelerBlock(AbstractBlock.Properties.create(Material.IRON)));
-
-
-
-    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
-        RegistryObject<T> toReturn = BLOCKS.register(name, block);
-
-        registerBlockItem(name, toReturn);
-
-        return toReturn;
-    }
-
-    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
-        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(ModItemGroup.MAIN_GROUP)));
-    }
-
-
-
-    public static void register(IEventBus eventBus) {
-        BLOCKS.register(eventBus);
-    }
-}
-*/
-
 public class ModBlocks {
     private static final CreateRegistrate REGISTRATE = Alloyed.getRegistrate().itemGroup(() -> {
         return ModItemGroup.MAIN_GROUP;
