21b06e8e78f09a2bf5d7f3775ae27ff7bedaca44
FortressNebula <prasun.nandan0909@gmail.com>
2022-01-13 17:05:41 +0000

    Proxy class for client registration

--- src/main/java/com/molybdenum/alloyed/Alloyed.java
+++ src/main/java/com/molybdenum/alloyed/Alloyed.java
@@ -1,6 +1,7 @@
 package com.molybdenum.alloyed;
 
 import com.molybdenum.alloyed.blocks.ModBlocks;
+import com.molybdenum.alloyed.client.ClientHandler;
 import com.molybdenum.alloyed.items.ModItems;
 import com.molybdenum.alloyed.sounds.ModSounds;
 import com.simibubi.create.foundation.data.CreateRegistrate;
@@ -28,40 +29,16 @@ public class Alloyed {
     public Alloyed() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-
         ModItems.register();
         ModBlocks.register();
         ModSounds.register(eventBus);
-        //ModTileEntities.register(eventBus);
-        //ModContainers.register(eventBus);
 
         eventBus.addListener(this::setupClient);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-
-
     private void setupClient(final FMLClientSetupEvent event) {
-        /*
-        RenderTypeLookup.setRenderLayer(ModFluids.OIL_FLUID.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(ModFluids.OIL_BLOCK.get(), RenderType.getTranslucent());
-        RenderTypeLookup.setRenderLayer(ModFluids.OIL_FLOWING.get(), RenderType.getTranslucent());
-        ScreenManager.registerFactory(ModContainers.LIGHTNING_CHANNELER_CONTAINER.get(), LightningChannelerScreen::new);
-        */
-
-        //*pain*
-        ItemProperties.register(ModItems.STEEL_FISHING_ROD.get(), new ResourceLocation("cast"), (stack, level, entity, i) -> {
-            if (entity == null) {
-                return 0.0F;
-            } else {
-                boolean isMainhand = entity.getMainHandItem() == stack;
-                boolean isOffHand = entity.getOffhandItem() == stack;
-                if (entity.getMainHandItem().getItem() instanceof FishingRodItem) {
-                    isOffHand = false;
-                }
-                return (isMainhand || isOffHand) && entity instanceof Player && ((Player) entity).fishing != null ? 1.0F : 0.0F;
-            }
-        });
+        ClientHandler.setupClient();
     }
 
     public static CreateRegistrate getRegistrate() {
---
+++ src/main/java/com/molybdenum/alloyed/client/ClientHandler.java
@@ -0,0 +1,29 @@
+package com.molybdenum.alloyed.client;
+
+import com.molybdenum.alloyed.Alloyed;
+import com.molybdenum.alloyed.items.ModItems;
+import net.minecraft.client.renderer.item.ItemProperties;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.FishingRodItem;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = Alloyed.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ClientHandler {
+    public static void setupClient() {
+        //*pain*
+        ItemProperties.register(ModItems.STEEL_FISHING_ROD.get(), new ResourceLocation("cast"), (stack, level, entity, i) -> {
+            if (entity == null) {
+                return 0.0F;
+            } else {
+                boolean isMainhand = entity.getMainHandItem() == stack;
+                boolean isOffHand = entity.getOffhandItem() == stack;
+                if (entity.getMainHandItem().getItem() instanceof FishingRodItem) {
+                    isOffHand = false;
+                }
+                return (isMainhand || isOffHand) && entity instanceof Player && ((Player) entity).fishing != null ? 1.0F : 0.0F;
+            }
+        });
+    }
+}
