b8ac017fb32cfe76e7ab4c36789d807370185482
FortressNebula <prasun.nandan0909@gmail.com>
2022-01-22 22:11:06 +0000

    v0.1b Release Commit

--- src/main/java/com/molybdenum/alloyed/Alloyed.java
+++ src/main/java/com/molybdenum/alloyed/Alloyed.java
@@ -6,21 +6,20 @@ import com.molybdenum.alloyed.items.ModItems;
 import com.molybdenum.alloyed.sounds.ModSounds;
 import com.simibubi.create.foundation.data.CreateRegistrate;
 import com.simibubi.create.repack.registrate.util.NonNullLazyValue;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FishingRodItem;
-import net.minecraft.item.ItemModelsProperties;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Mod(Alloyed.MOD_ID)
 public class Alloyed {
 
     public static final String MOD_ID = "alloyed";
+    public static final Logger LOGGER = LogManager.getLogger();
 
     // Registrate
     private static final NonNullLazyValue<CreateRegistrate> registrate = CreateRegistrate.lazy(MOD_ID);
@@ -39,6 +38,7 @@ public class Alloyed {
     private void setupClient(FMLClientSetupEvent event) {
         ClientHandler.setupClient();
     }
+
 
     // Registrate getter
     @SuppressWarnings("deprecation")
