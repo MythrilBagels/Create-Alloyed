2d0accf9c4383a3ac3ce4a167cdb1f5f7c0ecf80
FortressNebula <prasun.nandan0909@gmail.com>
2022-01-27 18:51:15 +0000

    new ponder

--- src/main/java/com/molybdenum/alloyed/ponder/BronzeBellPonder.java
+++ src/main/java/com/molybdenum/alloyed/ponder/BronzeBellPonder.java
@@ -3,9 +3,15 @@ package com.molybdenum.alloyed.ponder;
 import com.molybdenum.alloyed.Alloyed;
 import com.simibubi.create.foundation.ponder.SceneBuilder;
 import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
+import com.simibubi.create.foundation.ponder.elements.InputWindowElement;
+import com.simibubi.create.foundation.ponder.instructions.EmitParticlesInstruction;
+import com.simibubi.create.foundation.utility.Pointing;
+import net.minecraft.client.particle.EmitterParticle;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Direction;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.vector.Vector3d;
 
 public class BronzeBellPonder {
 
@@ -66,15 +72,34 @@ public class BronzeBellPonder {
     public static void instrument(SceneBuilder scene, SceneBuildingUtil util) {
         scene.title("bronze_bell_instrument", "Making Music with the Bronze Bell");
         scene.configureBasePlate(0, 0, 5);
+
+        // Useful positions
+        BlockPos bell = util.grid.at(2,1,2);
+        BlockPos noteblock = util.grid.at(2, 2, 2);
+        // ----------------
+
         scene.showBasePlate(); // Begin
         scene.idle(5);
 
         // Show the bronze bell
         scene.idle(5);
-        scene.world.showSection(util.select.position(2, 1, 2), Direction.DOWN);
+        scene.world.showSection(util.select.position(bell), Direction.DOWN);
         // Show the noteblock
         scene.idle(5);
-        scene.world.showSection(util.select.position(2, 2, 2), Direction.DOWN);
+        scene.world.showSection(util.select.position(noteblock), Direction.DOWN);
+
+        // Talk about bronze bells in general
+        scene.idle(30);
+        scene.overlay.showText(90)
+                .attachKeyFrame()
+                .text("Noteblocks placed above a Bronze Bell will play a special bell sound.")
+                .placeNearTarget()
+                .pointAt(util.vector.centerOf(noteblock));
+        scene.idle(100); // Wait for the text to go away
+
+        scene.overlay.showControls(new InputWindowElement(util.vector.centerOf(noteblock), Pointing.UP).rightClick(), 30);
+        scene.idle(10);
+        scene.effects.emitParticles(util.vector.blockSurface(noteblock, Direction.UP), EmitParticlesInstruction.Emitter.simple(ParticleTypes.NOTE, Vector3d.ZERO) , 1, 1);
 
         scene.idle(60); // End
     }
