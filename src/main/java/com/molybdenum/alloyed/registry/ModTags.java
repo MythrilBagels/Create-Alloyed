d4c69b1d2f3130de02d45787c6215be3e7181576
FortressNebula <prasun.nandan0909@gmail.com>
2022-01-27 17:32:04 +0000

    Redid tag system

--- src/main/java/com/molybdenum/alloyed/registry/ModTags.java
+++ src/main/java/com/molybdenum/alloyed/registry/ModTags.java
@@ -4,49 +4,52 @@ import com.molybdenum.alloyed.Alloyed;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.common.Tags;
 
 public class ModTags {
 
     public static class Items {
         // Make sure the shears are working
-        public static final Tags.IOptionalNamedTag<Item> STEEL_SHEARS = createForgeTag("shears");
+        public static final ITag.INamedTag<Item> STEEL_SHEARS = createForgeTag("shears");
 
         // Compat for other mods' ingots
-        public static final Tags.IOptionalNamedTag<Item> BRONZE_INGOT = createForgeTag("ingots/bronze");
-        public static final Tags.IOptionalNamedTag<Item> STEEL_INGOT = createForgeTag("ingots/steel");
+        public static final ITag.INamedTag<Item> BRONZE_INGOT = createForgeTag("ingots/bronze");
+        public static final ITag.INamedTag<Item> STEEL_INGOT = createForgeTag("ingots/steel");
 
         // Compat for other mods' sheets/plates
-        public static final Tags.IOptionalNamedTag<Item> BRONZE_SHEET = createForgeTag("plates/bronze");
-        public static final Tags.IOptionalNamedTag<Item> STEEL_SHEET = createForgeTag("plates/steel");
+        public static final ITag.INamedTag<Item> BRONZE_SHEET = createForgeTag("plates/bronze");
+        public static final ITag.INamedTag<Item> STEEL_SHEET = createForgeTag("plates/steel");
 
         // Compat for other mods' ingot blocks
-        public static final Tags.IOptionalNamedTag<Item> BRONZE_BLOCK = createForgeTag("storage_blocks/bronze");
-        public static final Tags.IOptionalNamedTag<Item> STEEL_BLOCK = createForgeTag("storage_blocks/steel");
+        public static final ITag.INamedTag<Item> BRONZE_BLOCK = createForgeTag("storage_blocks/bronze");
+        public static final ITag.INamedTag<Item> STEEL_BLOCK = createForgeTag("storage_blocks/steel");
 
-        private static Tags.IOptionalNamedTag<Item> createTag(String name) {
-            return ItemTags.createOptional(new ResourceLocation(Alloyed.MOD_ID, name));
+        // Compat for other mod' knives
+        public static final ITag.INamedTag<Item> STEEL_KNIFE = createForgeTag("tools/knives");
+
+        private static ITag.INamedTag<Item> createTag(String path) {
+            return ItemTags.bind(new ResourceLocation(Alloyed.MOD_ID, path).toString());
         }
 
-        private static Tags.IOptionalNamedTag<Item> createForgeTag(String name) {
-            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        private static ITag.INamedTag<Item> createForgeTag(String path) {
+            return ItemTags.bind(new ResourceLocation("forge", path).toString());
         }
     }
 
     public static class Blocks {
 
         // Compat for other mods' ingot blocks
-        public static final Tags.IOptionalNamedTag<Block> BRONZE_BLOCK = createForgeTag("storage_blocks/bronze");
-        public static final Tags.IOptionalNamedTag<Block> STEEL_BLOCK = createForgeTag("storage_blocks/steel");
+        public static final ITag.INamedTag<Block> BRONZE_BLOCK = createForgeTag("storage_blocks/bronze");
+        public static final ITag.INamedTag<Block> STEEL_BLOCK = createForgeTag("storage_blocks/steel");
 
-        private static Tags.IOptionalNamedTag<Block> createTag(String name) {
-            return BlockTags.createOptional(new ResourceLocation(Alloyed.MOD_ID, name));
+        private static ITag.INamedTag<Block> createTag(String name) {
+            return BlockTags.bind(new ResourceLocation(Alloyed.MOD_ID, name).toString());
         }
 
-        private static Tags.IOptionalNamedTag<Block> createForgeTag(String name) {
-            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        private static ITag.INamedTag<Block> createForgeTag(String name) {
+            return BlockTags.bind(new ResourceLocation("forge", name).toString());
         }
     }
 
