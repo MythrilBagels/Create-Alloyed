94464786b94ae9ffa3d101d2d36456fe5184c87d
FortressNebula <prasun.nandan0909@gmail.com>
2022-02-04 20:14:02 +0000

    Client Handler now uses @SubscribeEvent

--- src/main/java/com/molybdenum/alloyed/Alloyed.java
+++ src/main/java/com/molybdenum/alloyed/Alloyed.java
@@ -1,7 +1,6 @@
 package com.molybdenum.alloyed;
 
 import com.molybdenum.alloyed.registry.ModBlocks;
-import com.molybdenum.alloyed.client.ClientHandler;
 import com.molybdenum.alloyed.registry.ModItems;
 import com.molybdenum.alloyed.registry.ModSounds;
 import com.simibubi.create.foundation.data.CreateRegistrate;
@@ -9,18 +8,18 @@ import com.simibubi.create.repack.registrate.util.NonNullLazyValue;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 @Mod(Alloyed.MOD_ID)
 public class Alloyed {
 
     public static final String MOD_ID = "alloyed";
+    public static final Logger LOGGER = LogManager.getLogger();
 
     private static final NonNullLazyValue<CreateRegistrate> registrate = CreateRegistrate.lazy(MOD_ID);
-
-
 
     public Alloyed() {
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -29,17 +28,12 @@ public class Alloyed {
         ModBlocks.register();
         ModSounds.register(eventBus);
 
-        eventBus.addListener(this::setupClient);
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    private void setupClient(final FMLClientSetupEvent event) {
-        ClientHandler.setupClient();
+        LOGGER.debug("Finished registering items, blocks, and sounds!");
     }
 
     public static CreateRegistrate getRegistrate() {
         return registrate.get();
     }
-
 
 }
--- src/main/java/com/molybdenum/alloyed/client/ClientHandler.java
+++ src/main/java/com/molybdenum/alloyed/client/ClientHandler.java
@@ -7,12 +7,16 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.FishingRodItem;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = Alloyed.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientHandler {
-    public static void setupClient() {
-        //*pain*
+    @SubscribeEvent
+    public static void setupClient(final FMLClientSetupEvent event) {
+        Alloyed.LOGGER.debug("Entering client setup for Alloyed ...");
+        // Set up steel fishing rod
         ItemProperties.register(ModItems.STEEL_FISHING_ROD.get(), new ResourceLocation("cast"), (stack, level, entity, i) -> {
             if (entity == null) {
                 return 0.0F;
@@ -25,5 +29,7 @@ public class ClientHandler {
                 return (isMainhand || isOffHand) && entity instanceof Player && ((Player) entity).fishing != null ? 1.0F : 0.0F;
             }
         });
+
+        Alloyed.LOGGER.debug("Finished client setup for Alloyed");
     }
 }
