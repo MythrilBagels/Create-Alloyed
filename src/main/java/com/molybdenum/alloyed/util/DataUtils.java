f7fbc6c49996254f3c762401a19e7bd337817cf9
FortressNebula <prasun.nandan0909@gmail.com>
2022-02-04 20:15:25 +0000

    Update DataUtils.java

--- src/main/java/com/molybdenum/alloyed/util/DataUtils.java
+++ src/main/java/com/molybdenum/alloyed/util/DataUtils.java
@@ -2,14 +2,14 @@ package com.molybdenum.alloyed.util;
 
 import com.simibubi.create.repack.registrate.builders.BlockBuilder;
 import com.simibubi.create.repack.registrate.util.nullness.NonNullFunction;
+import net.minecraft.tags.Tag;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.common.Tags;
 
 public class DataUtils {
 
     public static <T extends Block, P> NonNullFunction<BlockBuilder<T, P>, BlockBuilder<T, P>> tagBlockAndItem(
-            Tags.IOptionalNamedTag<Block> blockTag, Tags.IOptionalNamedTag<Item> itemTag) {
+            Tag.Named<Block> blockTag, Tag.Named<Item> itemTag) {
         return b -> b
                 .tag(blockTag)
                 .item()
