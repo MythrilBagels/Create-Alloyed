b21240d1a2b687096e2ef0905f4b47b361100e65
Fortress Nebula <prasun.nandan0909@gmail.com>
2023-05-24 19:02:16 +0100

    added steel catwalk stairs

---
+++ src/main/java/com/molybdenum/alloyed/common/compat/createdeco/SteelCatwalkStairBlock.java
@@ -0,0 +1,9 @@
+package com.molybdenum.alloyed.common.compat.createdeco;
+
+import com.github.talrey.createdeco.blocks.CatwalkStairBlock;
+
+public class SteelCatwalkStairBlock extends CatwalkStairBlock {
+    public SteelCatwalkStairBlock(Properties props) {
+        super(props);
+    }
+}
