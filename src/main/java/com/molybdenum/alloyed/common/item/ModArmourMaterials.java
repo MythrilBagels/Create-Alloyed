fd277a494cb2dd764ab47fb32ba3d3cdf3f98ef8
Cassian Godsted <cassian@godsted.com>
2025-07-23 20:07:13 -0400

    buff horse armour a bit

--- src/main/java/com/molybdenum/alloyed/common/item/ModArmourMaterials.java
+++ src/main/java/com/molybdenum/alloyed/common/item/ModArmourMaterials.java
@@ -22,7 +22,7 @@ public class ModArmourMaterials {
 
     public static final Holder<ArmorMaterial> STEEL = register(
             "steel",
-            new int[] { 3, 5, 7, 3, 5},
+            new int[] { 3, 5, 7, 3, 7},
             10,
             SoundEvents.ARMOR_EQUIP_CHAIN,
             1.0F,
